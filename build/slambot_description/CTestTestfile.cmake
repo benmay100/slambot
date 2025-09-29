83aedc1348945808612b403a94d3d2e212cc48c4
Ben May <44981266+benmay100@users.noreply.github.com>
2025-09-29 17:16:02 +0100

    Delete build directory

--- build/slambot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ben-may/ros_workspaces/slambot_ws2/src/slambot_description
-# Build directory: /home/ben-may/ros_workspaces/slambot_ws2/src/build/slambot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
